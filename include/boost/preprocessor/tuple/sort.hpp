2dc0aa25a7ea0c60d64fc887658c8f107943f51b
Nicuvëo <crucuny@gmail.com>
2015-12-20 04:57:28 +0000

    wip

--- include/boost/preprocessor/tuple/sort.hpp
+++ include/boost/preprocessor/tuple/sort.hpp
@@ -16,6 +16,7 @@
 
 
 #  include <boost/preprocessor/array/lower_bound.hpp>
+#  include <boost/preprocessor/array/to_tuple.hpp>
 #  include <boost/preprocessor/array/insert.hpp>
 #  include <boost/preprocessor/tuple/elem.hpp>
 #  include <boost/preprocessor/tuple/size.hpp>
@@ -48,17 +49,12 @@
 #  define BOOST_PP_TS_PRED_0(D,S)   BOOST_PP_TS_PRED_1(D, BOOST_PP_TUPLE_ELEM(3,0,S), BOOST_PP_TUPLE_ELEM(3,1,S))
 #  define BOOST_PP_TS_PRED_1(D,T,I) BOOST_PP_LESS_D(D, I, BOOST_PP_TUPLE_SIZE(T))
 
-#  define BOOST_PP_TS_OP_0(D,S)         BOOST_PP_TS_OP_1(D, BOOST_PP_TUPLE_ELEM(3,0,S), BOOST_PP_TUPLE_ELEM(3,1,S), BOOST_PP_TUPLE_ELEM(3,2,S))
-#  define BOOST_PP_TS_OP_1(D,T,I,R)     BOOST_PP_TS_OP_2(D, T, I, R, BOOST_PP_TUPLE_ELEM(I, T))
-#  define BOOST_PP_TS_OP_2(D,T,I,R,X)   (T,                             \
-                                         BOOST_PP_INC(I),               \
-                                         BOOST_PP_ARRAY_INSERT_D(D, R, BOOST_PP_ARRAY_LOWER_BOUND_D(D,R,X), X))
-
+#  define BOOST_PP_TS_OP_0(  D,S)       BOOST_PP_TS_OP_1(  D, BOOST_PP_TUPLE_ELEM(3,0,S), BOOST_PP_TUPLE_ELEM(3,1,S), BOOST_PP_TUPLE_ELEM(3,2,S))
 #  define BOOST_PP_TS_OP_0_U(D,S)       BOOST_PP_TS_OP_1_U(D, BOOST_PP_TUPLE_ELEM(3,0,S), BOOST_PP_TUPLE_ELEM(3,1,S), BOOST_PP_TUPLE_ELEM(3,2,S))
+#  define BOOST_PP_TS_OP_1(  D,T,I,R)   BOOST_PP_TS_OP_2(  D, T, I, R, BOOST_PP_TUPLE_ELEM(I, T))
 #  define BOOST_PP_TS_OP_1_U(D,T,I,R)   BOOST_PP_TS_OP_2_U(D, T, I, R, BOOST_PP_TUPLE_ELEM(I, T))
-#  define BOOST_PP_TS_OP_2_U(D,T,I,R,X) (T,                             \
-                                         BOOST_PP_INC(I),               \
-                                         BOOST_PP_TS_MERGE_U(D, R, BOOST_PP_ARRAY_LOWER_BOUND_D(D,R,X), X))
+#  define BOOST_PP_TS_OP_2(  D,T,I,R,X) (T, BOOST_PP_INC(I), BOOST_PP_ARRAY_INSERT_D(D, R, BOOST_PP_ARRAY_LOWER_BOUND_D(D,R,X), X))
+#  define BOOST_PP_TS_OP_2_U(D,T,I,R,X) (T, BOOST_PP_INC(I),     BOOST_PP_TS_MERGE_U(D, R, BOOST_PP_ARRAY_LOWER_BOUND_D(D,R,X), X))
 
 
 
